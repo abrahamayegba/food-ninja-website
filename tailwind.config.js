3b758ad0106bedac87dc57404f80ccdda486b6f1
abraham <abrahamayegba@gmail.com>
2022-05-10 22:00:33 +0000

    ninja website

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: ['./dist/**/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        body: ['Nunito']
+      },
+      colors: {
+        primary: '#FF6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+        }
+      }
+    },
   },
   variants: {},
   plugins: [],
